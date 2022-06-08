e0409f4850a7fbfef1a18e61e0f28860c6aa19ca
glatterf42 <finkeben@web.de>
2022-06-08 12:34:59 +0200

    spectra might now support cross and power, ics and end from command line

--- spectra_computation.py
+++ spectra_computation.py
@@ -1,37 +1,71 @@
+#Call with spectra_computation.py <time> <kind> 
+#time = 'ics' for ICs, = 'end' for final results
+#kind = 'power' for power spectra comparing same resolution, 'cross' for comparing across all resolutions
+
+import itertools
+import re
 import subprocess
+from sys import argv
+import wave
 
 from paths import base_dir, spectra_dir
 
-Lbox = 100
-k0 = 2 * 3.14159265358979323846264338327950 / Lbox
-resolutions = [128, 256, 512] #leave out 512 for now, cause this might only work at the VSC
 
-spectra = spectra_dir / 'spectra'
+def spectra(waveform: str, resolution_1: int, resolution_2: int, Lbox: float, time: str):
+    setup_1 = f'{waveform}_{resolution_1}_{Lbox}'
+    setup_2 = f'{waveform}_{resolution_2}_{Lbox}'
 
-for k, waveform in enumerate(["DB2", "DB4", "DB8", "shannon"]):
-
-    for l, resolution in enumerate(resolutions):
-        setup = f'{waveform}_{resolution}_{Lbox}'
-        dir = base_dir / setup
+    # #For ICs: time == 'ics'
+    if time == 'ics':
         subprocess.run([str(spectra),
         '--ngrid',
-        # f'{2 * resolution}',
         '1024',
-        # #For evaluation of ICs
         '--format=4', #This seems to work, but is not as readable
         '--output',
-        str(base_dir / f'spectra/{waveform}_{Lbox}/{waveform}_{Lbox}_ics_{resolution}_{resolution}'),
+        str(base_dir / f'spectra/{waveform}_{Lbox}/{waveform}_{Lbox}_ics_{resolution_1}_{resolution_2}'),
         '--input',
-        str(base_dir / f'{setup}/ics_{setup}.hdf5'),
+        str(base_dir / f'{setup_1}/ics_{setup_1}.hdf5'),
         '--input',
-        str(base_dir / f'{setup}/ics_{setup}.hdf5')
-        
-        # #For evaluation of final results
-        # '--format=3', 
-        # '--output',
-        # str(base_dir / f'spectra/{waveform}_{Lbox}/{waveform}_{Lbox}_a4_{resolution}_{resolution}'),
-        # '--input',
-        # str(base_dir / f'{setup}/output_0004.hdf5'),
-        # '--input',
-        # str(base_dir / f'{setup}/output_0004.hdf5')
+        str(base_dir / f'{setup_2}/ics_{setup_2}.hdf5')
         ], check=True)
+    
+    # #For evaluation of final results: time == 'end'
+    if time == 'end':
+        subprocess.run([str(spectra),
+        '--ngrid',
+        '1024',
+        '--format=3', 
+        '--output',
+        str(base_dir / f'spectra/{waveform}_{Lbox}/{waveform}_{Lbox}_a4_{resolution_1}_{resolution_2}'),
+        '--input',
+        str(base_dir / f'{setup_1}/output_0004.hdf5'),
+        '--input',
+        str(base_dir / f'{setup_2}/output_0004.hdf5')
+        ], check=True)
+
+def power_run(waveforms: list, resolutions: list, Lbox: float, time: str):
+    for waveform in enumerate(waveforms):
+        for resolution in enumerate(resolutions):
+            spectra(waveform=waveform, resolution_1=resolution, resolution_2=resolution, Lbox=Lbox, time=time)
+
+def cross_run(waveforms: list, resolutions: list, Lbox: float, time:str):
+    for waveform in enumerate(waveforms):
+        for resolution_pair in list(itertools.combinations(resolutions, 2)):
+            spectra(waveform=waveform, resolution_1=resolution_pair[0], resolution_2=resolution_pair[1], Lbox=Lbox, time=time)
+
+
+if __name__ == '__main__':
+    Lbox = 100.0
+    k0 = 2 * 3.14159265358979323846264338327950 / Lbox
+    resolutions = [128, 256, 512] 
+    waveforms = ["DB2", "DB4", "DB8", "shannon"]
+
+    spectra = spectra_dir / 'spectra'
+    time = int(argv[1])
+
+    if argv[2] == 'power':
+        power_run(waveforms=waveforms, resolutions=resolutions, Lbox=Lbox, time=time)
+    
+    elif argv[2] == 'cross':
+        cross_run(waveforms=waveforms, resolutions=resolutions, Lbox=Lbox, time=time)
+
