d0a17b4f03c50e6ccd3645c87f7d8126c0702be0
Lukas Winkler <git@lw1.at>
2022-06-10 11:05:07 +0200

    also compare with same wf

--- compare_all.py
+++ compare_all.py
@@ -2,13 +2,14 @@ from compare_halos import compare_halo_resolutions
 
 for wf in ["DB2", "DB4", "DB8", "shannon"]:
     for res in [128, 256, 512]:
-        compare_halo_resolutions(
-            ref_waveform="shannon",
-            comp_waveform=wf,
-            reference_resolution=128,
-            comparison_resolution=res,
-            plot=False,
-            plot3d=False,
-            velo_halos=True,
-            single=False
-        )
+        for source_wf in [wf, "shannon"]:
+            compare_halo_resolutions(
+                ref_waveform=source_wf,
+                reference_resolution=128,
+                comp_waveform=wf,
+                comparison_resolution=res,
+                plot=False,
+                plot3d=False,
+                velo_halos=True,
+                single=False
+            )
