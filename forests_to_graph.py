f748886a960d5ce20d642f5b0b612ecd45342f88
Lukas Winkler <git@lw1.at>
2022-07-12 10:15:42 +0200

    very simple tree to graph tool

---
+++ forests_to_graph.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+print("digraph G {")
+# print("graph [ overlap=false ]")
+with open(argv[1]) as f:
+    next(f)
+    for line in f:
+        if line.startswith("#"): continue
+        cols = line.split()
+        if len(cols) < 5: continue
+        progenitor = int(cols[1])
+        descendant = int(cols[3])
+        if  descendant==-1: continue
+        print(f"  {progenitor} -> {descendant};")
+print("}")
