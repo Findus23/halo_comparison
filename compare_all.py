1279f3a7f19b328deddc71d0cd9912be5271a68c
Lukas Winkler <git@lw1.at>
2022-07-18 11:55:06 +0200

    better expections

--- compare_all.py
+++ compare_all.py
@@ -1,13 +1,15 @@
+import traceback
+
 from compare_halos import compare_halo_resolutions
 
 something_failed = False
 for wf in ["DB2", "DB4", "DB8", "shannon"]:
-    for res in [128, 256, 512]:
-        for source_wf in [wf, "shannon"]:
+    for res in [256, 512]:
+        for source_wf in [wf]:
             try:
                 compare_halo_resolutions(
                     ref_waveform=source_wf,
-                    reference_resolution=128,
+                    reference_resolution=256,
                     comp_waveform=wf,
                     comparison_resolution=res,
                     plot=False,
@@ -15,7 +17,8 @@ for wf in ["DB2", "DB4", "DB8", "shannon"]:
                     velo_halos=True,
                     single=False
                 )
-            except:
+            except Exception as e:
+                traceback.print_exc()
                 something_failed = True
                 continue
 
