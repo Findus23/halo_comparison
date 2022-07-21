dd53f051dddda5890d8e3c9f5ed9bb715ab45b95
Lukas Winkler <git@lw1.at>
2022-07-21 16:05:16 +0200

    get old comparison plot to work again

--- comparison_plot.py
+++ comparison_plot.py
@@ -4,32 +4,37 @@ import pandas as pd
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
+from halo_vis import get_comp_id
+from paths import base_dir
+
 num_bins = 5
 bins = np.geomspace(450, 80000, num_bins + 1)
-method = "shannon"
+waveform = "shannon"
+comparisons_dir = base_dir / "comparisons"
 
 
 def read(mode, ref_res, comp_res):
-    df = pd.read_csv(f"{mode}_{ref_res}_100_{mode}_{comp_res}_100.csv")
-    print(min(df.ref_masses), max(df.ref_masses))
+    df = pd.read_csv(comparisons_dir / get_comp_id(waveform, ref_res, waveform, comp_res))
+    # df = pd.read_csv(f"{mode}_{ref_res}_100_{mode}_{comp_res}_100.csv")
+    print(min(df.ref_Mvir), max(df.ref_Mvir))
 
-    digits = np.digitize(df.ref_masses, bins)
+    digits = np.digitize(df.ref_Mvir, bins)
     bin_means = []
     for i in range(num_bins):
         values = np.where(digits == i + 1)
         in_bin = df.iloc[values]
-        matches = np.array(in_bin.matches)
+        matches = np.array(in_bin.match)  # TODO: or instead fraction of halos that are matching?
         bin_means.append(matches.mean())
     return bin_means
 
 
 rows = [[1] * num_bins]
-resolutions = [128, 256, 512]
+resolutions = [128, 256, 512, 1024]
 ref_res = 128
 for res in resolutions:
     if res == ref_res:
         continue
-    means = read(method, 128, res)
+    means = read(waveform, 128, res)
     rows.append(means)
 
 data = np.array(rows).T
@@ -49,6 +54,6 @@ for x in range(data.shape[0]):
 p = ax.imshow(data, origin="lower", vmin=0.5, vmax=1)
 fig.colorbar(p)
 
-ax.set_title(method)
-fig.savefig(method + ".png")
+ax.set_title(waveform)
+# fig.savefig(method + ".png")
 plt.show()
